4a4d4eccc142139cccacf716b2f084fa4cdbbe2d
Julien Fieschi <julien.fieschi@afsr.fr>
2022-01-30 15:32:08 +0100

    Major Update.

--- InterAACtionPad/CalibrationView.swift
+++ InterAACtionPad/CalibrationView.swift
@@ -1,39 +1,32 @@
-////
-////  CalibrationView.swift
-////  InterAACtionPad
-////
-////  Created by Julien Fieschi on 30/01/2022.
-////
 //
-//import SwiftUI
+//  CalibrationView.swift
+//  InterAACtionPad
 //
-//struct DotCalibration: View {
-//    var body: some View {
-//        
-//        ZStack{
-//            Image(systemName: "dpad")
-////                .scaleEffect(10)
-////                .animation(Animation.linear(duration: 10).repeatForever(autoreverses: true))
-////                .animation(.easeInOut(duration: 10).repeatForever(autoreverses: true))
-////            Image(systemName: "dpad.fill")
-////                .animation(.easeOut(duration: 10).repeatForever(autoreverses: true))
-//        }
-//        
-//    }
-//}
+//  Created by Julien Fieschi on 30/01/2022.
 //
-//
-//
-//struct CalibrationView: View {
-//    var body: some View {
-//        
-//        DotCalibration()
-//        
-//    }
-//}
-//
-//struct CalibrationView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        CalibrationView()
-//    }
-//}
+
+import SwiftUI
+
+struct DotCalibration: View {
+    var body: some View {
+        
+        ZStack{
+            Image(systemName: "dpad")
+        }
+        
+    }
+}
+
+
+
+struct CalibrationView: View {
+    var body: some View {
+        DotCalibration()
+    }
+}
+
+struct CalibrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalibrationView()
+    }
+}
--- InterAACtionPad/ContentView.swift
+++ InterAACtionPad/ContentView.swift
@@ -9,23 +9,23 @@ import SwiftUI
 import WebKit
 //import EyeTrackKit
 
-//struct WebUIView: View{
-//
-//    //@ObservedObject var eyeTrackController: EyeTrackController = EyeTrackController(device: Device(type: .iPad), smoothingRange: 10, blinkThreshold: .infinity, isHidden: true)
-//    @State var url : URL
-//
-//    var body: some View{
-//        ZStack(alignment: .topLeading)
-//        {
-//            WebView(url: url)
-//            //eyeTrackController.view
-//            Circle()
-//                 .fill(Color.blue.opacity(0.5))
-//                 .frame(width: 25, height: 25)
-//                 //.position(x: eyeTrackController.eyeTrack.lookAtPoint.x, y: eyeTrackController.eyeTrack.lookAtPoint.y)
-//        }
-//    }
-//}
+struct WebUIView: View{
+
+    @ObservedObject var eyeTrackController: EyeTrackController = EyeTrackController(device: Device(type: .iPad), smoothingRange: 10, blinkThreshold: .infinity, isHidden: true)
+    @State var url : URL
+
+    var body: some View{
+        ZStack(alignment: .topLeading)
+        {
+            WebView(url: url)
+            eyeTrackController.view
+            Circle()
+                 .fill(Color.blue.opacity(0.5))
+                 .frame(width: 25, height: 25)
+                 .position(x: eyeTrackController.eyeTrack.lookAtPoint.x, y: eyeTrackController.eyeTrack.lookAtPoint.y)
+        }
+    }
+}
 
 struct WebView: UIViewRepresentable {
     typealias UIViewType = WKWebView
@@ -52,7 +52,7 @@ struct ContentView: View {
         NavigationView {
           List {
               NavigationLink {
-                  WebView(url: URL(string: "http://localhost:4000/index.html")!)
+                  WebUIView(url: URL(string: "http://localhost:4000/index.html")!)
               } label: {
                   HStack{
                       Text("AugCom")
@@ -63,7 +63,7 @@ struct ContentView: View {
                   }
               }
               NavigationLink {
-                  WebView(url: URL(string: "http://localhost:4000/InterAACtionScene/index.html")!)
+                  WebUIView(url: URL(string: "http://localhost:4000/InterAACtionScene/index.html")!)
               } label: {
                   HStack{
                       Text("Visual Scene")
@@ -74,7 +74,7 @@ struct ContentView: View {
                   }
               }
               NavigationLink {
-                  WebView(url: URL(string: "http://localhost:4000/InterAACtionPlayer/index.html")!)
+                  WebUIView(url: URL(string: "http://localhost:4000/InterAACtionPlayer/index.html")!)
               } label: {
                   HStack{
                       Text("Media Player")
@@ -91,7 +91,7 @@ struct ContentView: View {
                   Button(action: {
                    })
                   {
-                      NavigationLink(destination: WebView(url: URL(string: "http://localhost:4000/index.html")!)) {
+                      NavigationLink(destination: WebUIView(url: URL(string: "http://localhost:4000/index.html")!)) {
                               Image(systemName: "cursorarrow.motionlines.click")
                       }
                   }
